e90871834fbe4f2e66d5f94c62116d7313f0bfc1
ssm-incredible <sungmok.sohn@gmail.com>
2019-03-17 15:43:30 +0900

    anko 적용

--- app/src/main/java/com/bibimcode/ankoproject/MainActivity.kt
+++ app/src/main/java/com/bibimcode/ankoproject/MainActivity.kt
@@ -2,11 +2,27 @@ package com.bibimcode.ankoproject
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import org.jetbrains.anko.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        // setContentView(R.layout.activity_main)
+
+        verticalLayout {
+            padding = dip(30)
+            editText {
+                hint = "아이디"
+                textSize = 24f
+            }
+            editText {
+                hint = "비밀번호"
+                textSize = 24f
+            }
+            button("Login") {
+                textSize = 26f
+            }
+        }
     }
 }
